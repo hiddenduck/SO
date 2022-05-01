a4d7b98ecbaadb3dcee0977fad32cffaa09fedd6
Carlos Machado <74628557+hiddenduck@users.noreply.github.com>
2022-05-01 09:45:52 +0100

    Otimizar

--- Guião 4/4.c
+++ Guião 4/4.c
@@ -12,26 +12,19 @@ int main(int argc, char* argv[]){
     }
 
     char *Args[20];
-    int changeIN = 0, changeOut = 0, i, j;
+    int i, j;
     for(i=1, j=0; i<argc; i++){
-        if(strcmp(argv[i],"-i")==0)
-            changeIN = 1;
-        else if(strcmp(argv[i],"-o")==0)
-            changeOut = 1;
-        else if(changeIN){
-            fd_input = open(argv[i], O_RDONLY);
+        if(strcmp(argv[i],"-i")==0){
+            fd_input = open(argv[++i], O_RDONLY);
             dup2(fd_input, 0);
             close(fd_input);
-            changeIN = 0;
         }
-        else if(changeOut){
-            fd_output = open(argv[i], O_WRONLY|O_CREAT|O_TRUNC, 0600);
+        else if(strcmp(argv[i],"-o")==0){
+            fd_output = open(argv[++i], O_WRONLY|O_CREAT|O_TRUNC, 0600);
             dup2(fd_output, 1);
             close(fd_output);
-            changeOut = 0;
-        }else{
+        }else
             Args[j++] = argv[i];
-        }   
     }
 
     if(j!=0){
